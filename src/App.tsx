b54c9dfa3be597b9cb3ac69601ae1bc700588b86
Artem Mkrtchyan <mkr2263@yandex.ru>
2022-11-18 12:01:31 +0300

    delete app.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import { AppRouter } from './components/AppRouts'
 import { Header } from './components/Header/Header'
 import { Main } from './components/Main/Main'
